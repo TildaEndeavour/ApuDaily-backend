966fc21ba00d8ce8ef004b0dfa3712628a35820a
TildaEndeavour <bardakoff.v@gmail.com>
2024-08-14 19:25:42 +0300

    Init

---
+++ src/main/java/com/example/ApuDaily/ApuDailyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApuDaily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApuDailyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApuDailyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ApuDaily/ApuDailyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApuDaily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApuDailyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
