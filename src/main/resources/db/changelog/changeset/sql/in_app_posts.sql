0e74673fef8d74281848ab066df82856dde509b5
TildaEndeavour <bardakoff.v@gmail.com>
2024-08-15 16:45:39 +0300

    Liquibase initialization, add migration for posts

---
+++ src/main/resources/db/changelog/changeset/sql/in_app_posts.sql
@@ -0,0 +1,4 @@
+INSERT INTO posts (id, title, body, created_at) VALUES
+(1, 'In App Post #1', 'This is body of the in app post #1', '2024-08-15 01:00:00.0'),
+(2, 'In App Post #2', 'This is body of the in app post #2', '2024-08-15 02:00:00.0'),
+(3, 'In App Post #3', 'This is body of the in app post #3', '2024-08-15 03:00:00.0');
