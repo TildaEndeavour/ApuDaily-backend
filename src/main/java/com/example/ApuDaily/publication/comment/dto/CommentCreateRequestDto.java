b19e833a6314acecb690a2d53c8b6d8e3eddfb47
TildaEndeavour <bardakoff.v@gmail.com>
2025-10-07 22:41:16 +0300

    Comment create request dto

---
+++ src/main/java/com/example/ApuDaily/publication/comment/dto/CommentCreateRequestDto.java
@@ -0,0 +1,18 @@
+package com.example.ApuDaily.publication.comment.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentCreateRequestDto {
+    private Long postId;
+
+    private Long parentCommentId;
+
+    private Long authorId;
+
+    private String content;
+}
