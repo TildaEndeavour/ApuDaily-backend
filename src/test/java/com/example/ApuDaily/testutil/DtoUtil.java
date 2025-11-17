3e0f40edbb02fc006c7d72da383dec6950f1a805
TildaEndeavour <bardakoff.v@gmail.com>
2025-11-18 01:06:44 +0200

    UTC Timezone for tests

--- src/test/java/com/example/ApuDaily/testutil/DtoUtil.java
+++ src/test/java/com/example/ApuDaily/testutil/DtoUtil.java
@@ -13,12 +13,18 @@ public class DtoUtil {
 
   public PostCreateRequestDto postCreateRequestDto(
       int seed, Long categoryId, List<Long> tagsId, Long thumbnailId, User user) {
+
     return PostCreateRequestDto.builder()
         .authorId(user.getId())
         .thumbnailId(thumbnailId)
         .title("Publication title " + seed)
         .description("Publication description " + seed)
-        .content("Publication content " + seed)
+        .content(
+            "The standard Lorem Ipsum passage, used since the 1500s Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
+                + "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation "
+                + "ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum "
+                + "dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                + seed)
         .categoryId(categoryId)
         .tagsId(tagsId)
         .build();
